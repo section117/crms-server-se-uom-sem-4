6b8a868995e1666ea63fa56e6e974c8bc46b352c
SECTION <sec117tion@gmail.com>
2022-04-19 10:40:25 +0530

    Fixed a bug in Chats Router

--- routes/chat.js
+++ routes/chat.js
@@ -6,7 +6,7 @@ const chatsController = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddlewares');
 
 router.get('/', authMiddleware.ensureAuthenticated, chatsController.viewAllChats);
-router.get('/chat', chatController.viewChat);
+router.get('/chat', chatsController.viewChat);
 
 
 module.exports = router;

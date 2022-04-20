28e9418568d72f9a3294447f34953e8ee70792e1
SECTION <sec117tion@gmail.com>
2022-04-20 19:06:52 +0530

    Modified All Chats page

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,12 +4,5 @@ const viewAllChats = (req, res) => {
 	res.render('chats/all-chats.ejs');
 };
 
-const viewChat = (req, res) => {
-	// console.log(req.session.passport.user.id);
-	// console.log('welcome '+req.session.passport.user.first_name);
-	res.render('chat/chat.ejs');
-};
-
 exports.viewAllChats = viewAllChats;
-exports.viewChat = viewChat;
 
--- routes/chat.js
+++ routes/chat.js
@@ -6,7 +6,6 @@ const chatsController = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddlewares');
 
 router.get('/', authMiddleware.ensureAuthenticated, chatsController.viewAllChats);
-router.get('/chat', chatsController.viewChat);
 
 
 module.exports = router;

e2cb73e244aa121e435790885ad0999775e77f08
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-05-27 12:14:22 +0530

    Add input validation

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,20 +1,19 @@
-
 const viewHome = (req, res) => {
-	res.render('home/index.ejs');
+  res.render("home/index.ejs");
 };
 
 const viewSignupPage = (req, res) => {
-	res.render('home/register.ejs');
+  res.render("home/register.ejs");
 };
 
 const viewDashboard = (req, res) => {
-	// console.log(req.session.passport.user.id);
-	console.log('welcome '+req.session.passport.user.first_name);
-	res.render('home/dashboard.ejs');
+  // console.log(req.session.passport.user.id);
+  console.log("welcome " + req.session.passport.user.first_name);
+  res.render("home/dashboard.ejs", { user: req.session.passport.user });
 };
 
 module.exports = {
-	viewHome,
-	viewDashboard,
-	viewSignupPage
+  viewHome,
+  viewDashboard,
+  viewSignupPage,
 };
